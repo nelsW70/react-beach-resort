d2f6188eacb7577bb284a2aa042b6cbc8055388b
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-02 09:31:28 -0400

    add: single room setup

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../context';
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg
+    };
+  }
+
+  static contextType = RoomContext;
+
+  // componentDidMount() {}
+
   render() {
-    return <div>hello from single room page</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
+
+    return <div>hi</div>;
   }
 }
