de57fd3d0345a215b91e60067c3eb0c68b72b3fd
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-03 10:12:33 -0400

    add: Room List

--- src/components/RoomContainer.jsx
+++ src/components/RoomContainer.jsx
@@ -12,40 +12,11 @@ function RoomContainer({ context }) {
   }
 
   return (
-    <div>
-      hello from rooms container
+    <>
       <RoomFilter rooms={rooms} />
       <RoomsList rooms={sortedRooms} />
-    </div>
+    </>
   );
 }
 
 export default withRoomConsumer(RoomContainer);
-
-// import React from 'react';
-// import RoomFilter from './RoomFilter';
-// import RoomsList from './RoomsList';
-// import { RoomConsumer } from '../context';
-// import Loading from './Loading';
-
-// export default function RoomContainer() {
-//   return (
-//     <RoomConsumer>
-//       {value => {
-//         const { loading, sortedRooms, rooms } = value;
-
-//         if (loading) {
-//           return <Loading />;
-//         }
-
-//         return (
-//           <div>
-//             hello from rooms container
-//             <RoomFilter rooms={rooms} />
-//             <RoomsList rooms={sortedRooms} />
-//           </div>
-//         );
-//       }}
-//     </RoomConsumer>
-//   );
-// }
